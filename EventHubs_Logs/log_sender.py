b5db954b459357aebd8858fb08e9a70da2f3322f
magesh-1870 <magesh-1870@magesh-1870.csez.zohocorpin.com>
2019-11-15 11:35:02 +0530

    event hub function removed

--- EventHubs_Logs/log_sender.py
+++
@@ -1,8 +0,0 @@
-import json
-import azure.functions as func
-
-def main(event: func.EventHubEvent):
-    logging.info('S247 Function triggered to process a message: ', event.get_body())
-    logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
-    logging.info('  SequenceNumber =', event.sequence_number)
-    logging.info('  Offset =', event.offset)
