916884b4d58582d4013c7e53fedaf736ba7a3e97
magesh-1870 <magesh-1870@magesh-1870.csez.zohocorpin.com>
2019-11-21 07:05:13 +0530

    unwanted imports removed

--- EventHubs_Logs/log_sender.py
+++ EventHubs_Logs/log_sender.py
@@ -3,6 +3,3 @@ import azure.functions as func
 
 def main(event: func.EventHubEvent):
     logging.info('S247 Function triggered to process a message: %s', event.get_body().decode('utf-8'))
-    logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
-    logging.info('  SequenceNumber =', event.sequence_number)
-    logging.info('  Offset =', event.offset)
