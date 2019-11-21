8cf37d9dbbc0993f083f7c716e3baacc7f813b18
magesh-1870 <magesh-1870@magesh-1870.csez.zohocorpin.com>
2019-11-21 06:41:52 +0530

    logging info corrected

--- EventHubs_Logs/log_sender.py
+++ EventHubs_Logs/log_sender.py
@@ -2,7 +2,7 @@ import json, logging
 import azure.functions as func
 
 def main(event: func.EventHubEvent):
-    logging.info('S247 Function triggered to process a message: ', event.get_body())
+    logging.info('S247 Function triggered to process a message: %s', event.get_body().decode('utf-8'))
     logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
     logging.info('  SequenceNumber =', event.sequence_number)
     logging.info('  Offset =', event.offset)
